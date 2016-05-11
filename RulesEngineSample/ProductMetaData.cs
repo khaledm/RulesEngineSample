2787b1d147280cec67c3ddd44ba381d1923afe2b
Mahmud Khaled <mahmudkhaled@gmail.com>
2016-05-11 23:36:45 +0100

    first cut

---
+++ RulesEngineSample/ProductMetaData.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Linq.Expressions;
+
+namespace RulesEngineSample
+{
+
+    public class Rule
+    {
+        ///
+        /// Denotes the rules predictate (e.g. Name); comparison operator(e.g. ExpressionType.GreaterThan); value (e.g. "Cole")
+        /// 
+        public string ComparisonPredicate { get; set; }
+        public ExpressionType ComparisonOperator { get; set; }
+        public string ComparisonValue { get; set; }
+
+        /// 
+        /// The rule method that 
+        /// 
+        public Rule(string comparisonPredicate, ExpressionType comparisonOperator, string comparisonValue)
+        {
+            ComparisonPredicate = comparisonPredicate;
+            ComparisonOperator = comparisonOperator;
+            ComparisonValue = comparisonValue;
+        }
+    }
+
+    public class ProductMetaData
+    {
+        public decimal MaxSumAssured { get; set; }
+        public decimal MinSumAssured { get; set; }
+        public int ProductCode { get; set; }
+
+        public int ProductVersion { get; set; }
+
+        public int MinPremium { get; set; }
+
+        public int MinEntryAge { get; set; }
+
+    }
+
+    public class PrecompiledRules
+    {
+        ///
+        /// A method used to precompile rules for a provided type
+        /// 
+        public static List<Func<T, bool>> CompileRule<T>(List<T> targetEntity, List<Rule> rules)
+        {
+            var compiledRules = new List<Func<T, bool>>();
+
+            // Loop through the rules and compile them against the properties of the supplied shallow object 
+            rules.ForEach(rule =>
+            {
+                var genericType = Expression.Parameter(typeof(T));
+                var key = MemberExpression.Property(genericType, rule.ComparisonPredicate);
+                var propertyType = typeof(T).GetProperty(rule.ComparisonPredicate).PropertyType;
+                var value = Expression.Constant(Convert.ChangeType(rule.ComparisonValue, propertyType));
+                var binaryExpression = Expression.MakeBinary(rule.ComparisonOperator, key, value);
+
+                compiledRules.Add(Expression.Lambda<Func<T,bool>>(binaryExpression, genericType).Compile());
+            });
+
+            // Return the compiled rules to the caller
+            return compiledRules;
+        }
+    }
+}
--- RulesEngineSample/Program.cs
+++ RulesEngineSample/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace RulesEngineSample
 {
@@ -10,6 +9,35 @@ namespace RulesEngineSample
     {
         static void Main(string[] args)
         {
+            List<Rule> rules = new List<Rule>
+            {
+                 // Create some rules using LINQ.ExpressionTypes for the comparison operators
+                 new Rule ( "MaxSumAssured", ExpressionType.Equal, "250000"),
+                 new Rule ( "ProductCode", ExpressionType.Equal, "2001"),
+                 new Rule ( "MinEntryAge", ExpressionType.Equal, "59" )
+
+            };
+
+            var compiledMakeModelYearRules = PrecompiledRules.CompileRule(new List<ProductMetaData>(), rules);
+
+            var productMetaData = new List<ProductMetaData>() {
+                new ProductMetaData() { MaxSumAssured = 250000, ProductCode=2001, ProductVersion=1, MinSumAssured=1500, MinEntryAge=18, MinPremium=5 },
+                new ProductMetaData() { MaxSumAssured = 150000, ProductCode=2001, ProductVersion=2, MinSumAssured=1500, MinEntryAge=18, MinPremium=5 },
+                new ProductMetaData() { MaxSumAssured = 50000, ProductCode=2001, ProductVersion=3, MinSumAssured=1500, MinEntryAge=18, MinPremium=5 },
+                new ProductMetaData() { MaxSumAssured = 500000, ProductCode=2001, ProductVersion=4, MinSumAssured=1500, MinEntryAge=18, MinPremium=5 },
+                new ProductMetaData() { MaxSumAssured = 450000, ProductCode=2001, ProductVersion=5, MinSumAssured=1500, MinEntryAge=18, MinPremium=5 }
+            };
+
+            productMetaData.ForEach(pm => {
+                if (compiledMakeModelYearRules.TakeWhile(i => i(pm)).Count() > 0){
+                    Console.WriteLine("PM model: {0} {1} Passed the compiled rules engine check!", pm.ProductCode, pm.ProductVersion);
+                }
+                else
+                {
+                    Console.WriteLine("PM model: {0} {1} Failed!", pm.ProductCode, pm.ProductVersion);
+                }
+            });
+
         }
     }
 }
